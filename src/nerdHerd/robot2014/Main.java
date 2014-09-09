7a2f6a2aac8210f1bf8f74f2a04995b03631d6a9
Theodore A. Harder <taharder@gmail.com>
2014-09-08 18:53:57 -0700

    merged changes from cocker-spanielv2 (Chezy Champs working code)

--- src/nerdHerd/robot2014/Main.java
+++ src/nerdHerd/robot2014/Main.java
@@ -16,14 +16,10 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Main extends NerdyBot{
     Joystick leftJoystick, rightJoystick, articJoystick;
-    NerdyTimer autonomousTimer = new NerdyTimer(2.0), autonomousShooterTimer = new NerdyTimer(5.0);
     PainTrain myRobot;
     Timer timer;
     //KinectStick leftStick, rightStick;
     boolean shoot = false;
-    
-    
-    double leftSpeed = 0.0, rightSpeed = 0.0;
     boolean isRunningAutonomous = false;
     
 
@@ -32,14 +28,23 @@ public class Main extends NerdyBot{
         leftJoystick    = new Joystick(1);
         rightJoystick   = new Joystick(2);
         articJoystick   = new Joystick(3);
-        autonomousTimer.start();
-        timer           = new Timer();
+        timer           = new Timer();  // timer.get() == 0
         //leftStick = new KinectStick(1);
         //rightStick = new KinectStick(2);
-        autonomousShooterTimer.start();
 
     }
 
+    public void autonomousInit(){
+        myRobot.enable();
+        myRobot.shift(ThreeCimBallShifter.GearNumber.kFirstGear);
+        timer.start();
+    }
+    
+    public void autonomousContinous(){
+       myRobot.run();
+
+    }
+    
     public void autonomousPeriodic() {
         myRobot.setLeft(-0.5);
         myRobot.setRight(-0.5);
@@ -48,35 +53,26 @@ public class Main extends NerdyBot{
             myRobot.setRight(0.0);
             timer.stop();
         }
+        report();
     }
-    
-    public void autonomousContinous(){
-       myRobot.run();
-       report('A');
-    }
-    
-    public void autonomousInit(){
-        myRobot.enable();
-        myRobot.shift(ThreeCimBallShifter.GearNumber.kFirstGear);
-        timer.start();
-        timer.reset();
-    }
+   
     
     public void teleopInit() {
         myRobot.enable();
-        timer.start();
         timer.reset();
     }
-
+    
+    public void teleopContinous(){
+        myRobot.run();
+    }
+ 
     public void teleopPeriodic() {
-        leftSpeed           =  leftJoystick.getY();
-        rightSpeed          =  rightJoystick.getY();
         boolean shiftUp     =  leftJoystick.getRawButton(3);
         boolean shiftDown   =  leftJoystick.getRawButton(2);
         boolean intakeIn    =  articJoystick.getRawButton(11);
         boolean intakeOut   =  articJoystick.getRawButton(10);
-        boolean intake      =  articJoystick.getRawButton(3);
-        boolean outTake     =  articJoystick.getRawButton(2);
+        boolean intakeMotor      =  articJoystick.getRawButton(3);
+        boolean outTakeMotor     =  articJoystick.getRawButton(2);
         boolean shoot1       =  articJoystick.getRawButton(6);
         boolean shoot2      = articJoystick.getRawButton(4);
         boolean shoot3      = articJoystick.getRawButton(1);
@@ -88,9 +84,9 @@ public class Main extends NerdyBot{
             myRobot.extendIntake();
         }
 
-        if(intake){
+        if(intakeMotor){
             myRobot.startIntake(1.0);
-        }else if(outTake){
+        }else if(outTakeMotor){
             myRobot.startIntake(-1.0);
         }else{
             myRobot.stopIntake();
@@ -111,15 +107,13 @@ public class Main extends NerdyBot{
             myRobot.shoot(0.6);
         }
         
-        myRobot.   setLeft(leftSpeed);
-        myRobot.   setRight(rightSpeed);
+        myRobot.   setLeft(leftJoystick.getY());
+        myRobot.   setRight(rightJoystick.getY());
         
-        report('T');
+        report();
     }
     
-    public void teleopContinous(){
-        myRobot.run();
-    }
+   
     
     public void disabledInit(){
         myRobot.disable();
@@ -128,11 +122,10 @@ public class Main extends NerdyBot{
     
     public void disabled(){
         myRobot.run();
-        report('D');
+        report();
     }
     
-    private void report(char robotState)
-    {                
+    private void report(){                
         double time = timer.get();
         SmartDashboard.putNumber("Encoder Left", myRobot.getLeftGBEncoder());
         SmartDashboard.putNumber("Encoder Right", myRobot.getRightGBEncoder());
